423e3c251a0a65132b32e4e0f76a193e0a886e07
Admin <Admin@Admin>
2024-06-18 15:12:06 +0530

    1st Biuld

---
+++ src/test/java/Test/LoginTest.java
@@ -0,0 +1,11 @@
+package Test;
+
+public class LoginTest
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hiii");
+		System.out.println("Hello");
+		System.out.println("byee");
+	}
+}
